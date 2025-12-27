38fa6c954926ed7c5e58e3855ed40e662d92ef57
betulltok <betultok6181@gmail.com>
2025-12-27 21:28:07 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,19 @@
+# src/model.py
 
+import torch.nn as nn
+from torchvision import models
+
+def get_model(num_classes):
+    """
+    ResNet18 tabanlı sınıflandırma modeli
+    """
+    model = models.resnet18(weights="IMAGENET1K_V1")
+
+    # Önceden öğrenilmiş katmanları dondur
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Son katmanı değiştir
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+    return model
